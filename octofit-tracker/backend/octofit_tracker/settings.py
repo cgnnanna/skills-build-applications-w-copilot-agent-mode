e14497ad304cde43e9cbf19e68e6b2e17a5931d7
Chinyere Gloria Maduekwe <cgnnanna@gmail.com>
2025-11-13 03:26:23 +0000

    Implement Django app structure with models, serializers, views, and API routing for user management and activities

--- octofit-tracker/backend/octofit_tracker/settings.py
+++ octofit-tracker/backend/octofit_tracker/settings.py
@@ -25,10 +25,13 @@ SECRET_KEY = 'django-insecure-o*k1hd5@=1^+s=v)ncc(ogw3=_e73hwl_3h^kizccg#ulw=p1q
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = []
+
+# Allow all hosts
+ALLOWED_HOSTS = ['*']
 
 
 # Application definition
+
 
 INSTALLED_APPS = [
     'django.contrib.admin',
@@ -37,9 +40,15 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
+    'octofit_tracker',
+    'rest_framework',
+    'djongo',
+    'corsheaders',
 ]
 
+
 MIDDLEWARE = [
+    'corsheaders.middleware.CorsMiddleware',
     'django.middleware.security.SecurityMiddleware',
     'django.contrib.sessions.middleware.SessionMiddleware',
     'django.middleware.common.CommonMiddleware',
@@ -73,10 +82,17 @@ WSGI_APPLICATION = 'octofit_tracker.wsgi.application'
 # Database
 # https://docs.djangoproject.com/en/4.1/ref/settings/#databases
 
+
+# Djongo configuration for MongoDB
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
+        'ENGINE': 'djongo',
+        'NAME': 'octofit_db',
+        'ENFORCE_SCHEMA': False,
+        'CLIENT': {
+            'host': 'localhost',
+            'port': 27017,
+        },
     }
 }
 
@@ -113,9 +129,16 @@ USE_TZ = True
 
 
 # Static files (CSS, JavaScript, Images)
+# Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/4.1/howto/static-files/
 
 STATIC_URL = 'static/'
+
+# CORS settings
+CORS_ALLOW_ALL_ORIGINS = True
+CORS_ALLOW_CREDENTIALS = True
+CORS_ALLOW_HEADERS = ['*']
+CORS_ALLOW_METHODS = ['DELETE', 'GET', 'OPTIONS', 'PATCH', 'POST', 'PUT']
 
 # Default primary key field type
 # https://docs.djangoproject.com/en/4.1/ref/settings/#default-auto-field
---
+++ octofit-tracker/backend/octofit_tracker/tests.py
@@ -0,0 +1,23 @@
+from django.test import TestCase
+from .models import User, Team, Activity, Workout, Leaderboard
+
+class ModelTests(TestCase):
+    def setUp(self):
+        marvel = Team.objects.create(name='Marvel')
+        dc = Team.objects.create(name='DC')
+        user1 = User.objects.create(name='Iron Man', email='ironman@marvel.com', team=marvel)
+        user2 = User.objects.create(name='Batman', email='batman@dc.com', team=dc)
+        Activity.objects.create(user=user1, type='Running', duration=30, date='2025-11-13')
+        Workout.objects.create(name='Hero Training', description='Intense workout for heroes')
+        Leaderboard.objects.create(team=marvel, points=100)
+        Leaderboard.objects.create(team=dc, points=80)
+    def test_user_count(self):
+        self.assertEqual(User.objects.count(), 2)
+    def test_team_count(self):
+        self.assertEqual(Team.objects.count(), 2)
+    def test_activity_count(self):
+        self.assertEqual(Activity.objects.count(), 1)
+    def test_workout_count(self):
+        self.assertEqual(Workout.objects.count(), 1)
+    def test_leaderboard_count(self):
+        self.assertEqual(Leaderboard.objects.count(), 2)
